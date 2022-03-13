439bc37276d57e81399dac40effb688293cd05ed
Alexandrade01 <solidwinter96@outlook.com>
2022-03-13 20:02:18 -0300

    gerando um novo projeto pelo spring initialzr

---
+++ src/main/java/academy/devdojo/Myspringboot2essentials/MySpringboot2EssentialsApplication.java
@@ -0,0 +1,13 @@
+package academy.devdojo.Myspringboot2essentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringboot2EssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringboot2EssentialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academy/devdojo/Myspringboot2essentials/MySpringboot2EssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.devdojo.Myspringboot2essentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringboot2EssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
