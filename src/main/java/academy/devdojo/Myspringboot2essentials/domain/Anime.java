95b9bee9b8540f3700f3095fe1c14303864ab4e7
Alexandrade01 <solidwinter96@outlook.com>
2022-03-15 14:07:52 -0300

    criacao endpoint save

--- src/main/java/academy/devdojo/Myspringboot2essentials/domain/Anime.java
+++ src/main/java/academy/devdojo/Myspringboot2essentials/domain/Anime.java
@@ -1,5 +1,7 @@
 package academy.devdojo.Myspringboot2essentials.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,6 +9,9 @@ import lombok.Data;
 @AllArgsConstructor
 public class Anime {
     private Long id;
+    
+    //jsonproperty é uma mascara para apresentar na saida
+    //@JsonProperty("Nome do anime")
     private String name;
 
 }
--- src/main/java/academy/devdojo/Myspringboot2essentials/service/AnimeService.java
+++ src/main/java/academy/devdojo/Myspringboot2essentials/service/AnimeService.java
@@ -1,6 +1,8 @@
 package academy.devdojo.Myspringboot2essentials.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -10,8 +12,14 @@ import academy.devdojo.Myspringboot2essentials.domain.Anime;
 
 @Service
 public class AnimeService {
-	private final List<Anime> animes = List.of(new Anime(1L, "Dragon Ball Z"), new Anime(2L, "Berserk"), new Anime(3L,"Boruto"));
-    // private final AnimeRepository animeRepository;
+	
+	private final List<Anime> animes = new ArrayList<>
+	(
+		List.of(new Anime(1L, "Dragon Ball Z"), new Anime(2L, "Berserk"), new Anime(3L,"Boruto"))	
+	);
+	
+	// private final AnimeRepository animeRepository;
+	
     public List<Anime> listAll() {
         return animes;
     }
@@ -22,5 +30,12 @@ public class AnimeService {
 				.filter(anime -> anime.getId().equals(id))
 				.findFirst()
 				.orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Anime not found!"));
+	}
+
+	public Anime save(Anime animeBody) {
+		//setando o id automaticamente do 4 ao 100.000
+		animeBody.setId(ThreadLocalRandom.current().nextLong(4, 100000));
+		animes.add(animeBody);
+		return animeBody;
 	}
 }
