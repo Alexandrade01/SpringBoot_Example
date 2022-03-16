4c1e3a0cd454f3336fd2c0f38101e76922b4977b
Alexandrade01 <solidwinter96@outlook.com>
2022-03-16 18:11:48 -0300

    utilizando o FrameWork de Mapeamento MapStruct

--- src/main/java/academy/devdojo/Myspringboot2essentials/mapper/AnimeMapper.java
+++ src/main/java/academy/devdojo/Myspringboot2essentials/mapper/AnimeMapper.java
@@ -1,20 +1,16 @@
 package academy.devdojo.Myspringboot2essentials.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
-
 import academy.devdojo.Myspringboot2essentials.domain.Anime;
 import academy.devdojo.Myspringboot2essentials.request.AnimePostRequestBodyDTO;
 import academy.devdojo.Myspringboot2essentials.request.AnimePutRequestBodyDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public abstract class AnimeMapper {
-	
-	public static final AnimeMapper INSTANCE = Mappers.getMapper(AnimeMapper.class);
-	
-	public abstract Anime toAnime(AnimePostRequestBodyDTO animePostRequestBodyDTO);
-	
-	public abstract Anime toAnime(AnimePutRequestBodyDTO animePutRequestBodyDTO);
-	
-	
+    public static final AnimeMapper INSTANCE = Mappers.getMapper(AnimeMapper.class);
+
+    public abstract Anime toAnime(AnimePostRequestBodyDTO AnimePostRequestBodyDTO);
+
+    public abstract Anime toAnime(AnimePutRequestBodyDTO animePutRequestBodyDTO);
 }
